8fb938cd5cb888a3c38d080658daa511fe9c71bf
hana <rlagksk8428@gmail.com>
2022-10-18 18:19:23 +0900

    fix: modify login page ui

--- FrontEnd/src/pages/Auth/LoginPage/Login.tsx
+++ FrontEnd/src/pages/Auth/LoginPage/Login.tsx
@@ -1,97 +1,85 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const Wrap = styled.div`
+  @import '~antd/dist/antd.css';
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Content = styled.div`
-  width: 510;
-  height: 735;
-`
-
-const TItleWrap = styled.div`
-  margin-bottom: 119px;
-`
+  width: 518px;
+  height: 610px;
+  padding: 80px 118px;
+  box-shadow: 0px 1px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
 
 const Title = styled.div`
   font-style: normal;
-  font-weight: 800;
-  font-size: 32px;
-  color: #444b59;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`
-const InputLabel = styled.label`
-  font-style: normal;
-  font-weight: 550;
-  font-size: 16px;
-  letter-spacing: 0.1em;
-  color: #444b59;
-  margin-bottom: 12px;
-`;
-  
-const Input = styled.input`
-  padding: 5px 50px 5px 30px;
-  gap: 10px;
-  width: 213px;
-  height: 32px;
-  background: #ffffff;
-  border: 2px solid #789ade;
-  border-radius: 50px;
-  margin-bottom: 30px;
-  font-size: 15px;
-  line-height: 33px;
-  letter-spacing: 0.1em;
-  &::placeholder {
-    color: #c8d3f9;
-  }
-`;
-
-const Button = styled.button`
-  width: 300px;
-  height: 55px;
-  background: #8699da;
-  box-shadow: 4px 8px 12px rgba(78, 99, 141, 0.06), inset 0px 4px 12px rgba(200, 211, 249, 0.6);
-  border-radius: 80px;
-  border: none;
   font-weight: 600;
-  font-size: 20px;
-  color: #ffffff;
-  margin-top: 110px;
-  cursor: pointer;
+  font-size: 23px;
+  color: black;
+  margin-bottom: 100px;
+  text-align: center;
+  font-family: 'Noto Sans', sans-serif;
 `;
 
+const StyledButton = styled(Button)`
+  background: #0078f0;
+  border-radius: 5px;
+  margin-top: 70px;
+`;
+
+const StyledInput = styled(Form.Item)`
+  margin-bottom: 40px;
+`
 
 export default function Login() {
+  
+  const navigate = useNavigate();
 
- const {
-   register,
-   handleSubmit,
- } = useForm();
+ const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+    //인증토큰 로그인 기능 추가
+  };
 
   return (
     <Wrap>
       <Content>
-        <TItleWrap>
-          <Title>M-HEALTH</Title>
-        </TItleWrap>
-        <Form onSubmit={handleSubmit((res) => res)}>
-          <InputLabel>User Id</InputLabel>
-          <Input {...register('id', { required: true })} />
-          <InputLabel>Password</InputLabel>
-          <Input {...register('password', { required: true })} />
+        <Title>LaGonave - Mhealth</Title>
+        <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+          <StyledInput name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </StyledInput>
+          <StyledInput name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+            <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
+          </StyledInput>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item>
+            <StyledButton
+              type="primary"
+              block
+              htmlType="submit"
+              className="login-form-button"
+              onClick={() => navigate("/patient/first-form")}
+            >
+              Log in
+            </StyledButton>
+          </Form.Item>
         </Form>
-        <Button type="submit">Log in</Button>
       </Content>
     </Wrap>
   );
-}
+};
