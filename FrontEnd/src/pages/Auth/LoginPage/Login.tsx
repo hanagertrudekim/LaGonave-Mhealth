c6626b68dfad17a5a2fea12b067f4a7e100dc201
hana <rlagksk8428@gmail.com>
2022-10-21 03:36:48 +0900

    feat: add login option

--- FrontEnd/src/pages/Auth/LoginPage/Login.tsx
+++ FrontEnd/src/pages/Auth/LoginPage/Login.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button,  Form, Input, Radio, RadioChangeEvent } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 const Wrap = styled.div`
-  @import '~antd/dist/antd.css';
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -26,15 +25,23 @@ const Title = styled.div`
   font-weight: 600;
   font-size: 23px;
   color: black;
-  margin-bottom: 100px;
+  margin-bottom: 85px;
   text-align: center;
   font-family: 'Noto Sans', sans-serif;
 `;
 
+const StyledRadioGroup = styled(Radio.Group)`
+  margin-bottom: 18px;
+`
+
+const StyledRadio = styled(Radio)`
+  font-size: 13px;
+`
+
 const StyledButton = styled(Button)`
   background: #0078f0;
   border-radius: 5px;
-  margin-top: 70px;
+  margin-top: 80px;
 `;
 
 const StyledInput = styled(Form.Item)`
@@ -42,41 +49,62 @@ const StyledInput = styled(Form.Item)`
 `
 
 export default function Login() {
+
+  const [value, setValue] = useState(1);
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState({});
   
   const navigate = useNavigate();
 
- const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-    //인증토큰 로그인 기능 추가
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  const onFinish = (values: any) => {
+      console.log('Received values of form: ', values);
+      //인증토큰 로그인 기능 추가
+    };
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
   };
 
   return (
     <Wrap>
       <Content>
         <Title>LaGonave - Mhealth</Title>
-        <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+        <StyledRadioGroup onChange={onChange} value={value}>
+          <StyledRadio value={1}>Church-staff</StyledRadio>
+          <StyledRadio value={2}>Doctor</StyledRadio>
+        </StyledRadioGroup>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
+        >
           <StyledInput name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
             <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
           </StyledInput>
           <StyledInput name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
             <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
           </StyledInput>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-          </Form.Item>
-
-          <Form.Item>
-            <StyledButton
-              type="primary"
-              block
-              htmlType="submit"
-              className="login-form-button"
-              onClick={() => navigate("/patient/first-form")}
-            >
-              Log in
-            </StyledButton>
+          <Form.Item shouldUpdate>
+            {() => (
+              <StyledButton
+                type="primary"
+                block
+                htmlType="submit"
+                disabled={
+                  !form.isFieldsTouched(true) || !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                }
+                onClick={() => (value === 1 ? navigate('/identification') : navigate('/'))}
+              >
+                Log in
+              </StyledButton>
+            )}
           </Form.Item>
         </Form>
       </Content>
