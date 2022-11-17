48924c39990d8f6c3331d4db34614a1ce0d37912
hana <rlagksk8428@gmail.com>
2022-11-17 23:54:46 +0900

    feat: add logger, delete header token

--- FrontEnd/src/store/api/authApi.ts
+++ FrontEnd/src/store/api/authApi.ts
@@ -1,22 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { RootState } from '..';
 import { API_PATH } from '../../constant';
+
+console.log(API_PATH)
 
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
     baseUrl: `${API_PATH}`,
-    prepareHeaders: (headers, { getState }) => {
-      const { token } = (getState() as RootState).auth;
-      if (token) {
-        headers.set('authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  }), 
   endpoints: (builder) => ({
     loginUser: builder.mutation({
-      query: (body: { username: string; password: string}) => {
+      query: (body: {username: string; password: string}) => {
         return ({
           url: '/api/auth/login',
           method: 'POST',
--- FrontEnd/src/store/index.tsx
+++ FrontEnd/src/store/index.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { authApi } from './api/authApi';
+import logger from 'redux-logger';
 import authReducer from './slice/authSlice'
 
 export const store = configureStore({
@@ -8,12 +9,13 @@ export const store = configureStore({
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger, authApi.middleware),
+});
 
 export type AppDispatch = typeof store.dispatch;
 
 export type RootState = ReturnType<typeof store.getState>;
 
 setupListeners(store.dispatch)
+
+export default store;
