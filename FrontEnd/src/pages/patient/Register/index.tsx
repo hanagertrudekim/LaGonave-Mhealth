8ecd77a88d3cc1e686e5423182f7148d140d7ccc
hana <rlagksk8428@gmail.com>
2022-11-24 19:26:13 +0900

    feat: add customform patient register form

--- FrontEnd/src/pages/patient/Register/index.tsx
+++ FrontEnd/src/pages/patient/Register/index.tsx
@@ -1,22 +1,12 @@
-import React from 'react'
-import {  Col,  Input, InputNumber, Radio, Row, Select} from 'antd';
+import React from 'react';
 import useStep from '../useStep';
-import { Option } from 'antd/lib/mentions';
 import { useNavigate } from 'react-router-dom';
-import * as S from './Register.style'
+import * as S from './Register.style';
+import { REGISTER_VALIDATE_MESSAGE } from '../../../constant/validateMessage';
+import CustomForm from '../../../components/common/CustomForm';
+import { GENDER_OPTION } from '../../../constant';
 
 export default function RegisterForm() {
-  /* eslint-disable no-template-curly-in-string */
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      number: '${label} is not a valid number!',
-    },
-    number: {
-      range: '${label} must be between ${min} and ${max}',
-    },
-  };
-
   const { StepComponents } = useStep();
   const navigate = useNavigate();
 
@@ -26,65 +16,75 @@ export default function RegisterForm() {
     navigate('/patient/diagnosis-form');
   };
 
-  const prefixSelector = (
-    <S.StyledFormItem name="prefix" noStyle>
-      <Select style={{ width: 70 }} defaultValue="+86">
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    </S.StyledFormItem>
-  );
-
   return (
     <S.Wrap>
       <S.StepWrap>
         <StepComponents />
       </S.StepWrap>
-      <S.StyledForm name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-        <S.StyledFormItem name={['user', 'name']} label="Name" rules={[{ required: true }]}>
-          <Row gutter={8}>
-            <Col span={12}>
-              <Input placeholder="last name" />
-            </Col>
-            <Col span={11}>
-              <Input placeholder="first name" />
-            </Col>
-          </Row>
-        </S.StyledFormItem>
-        <S.StyledFormItem name={['user', 'gender']} label="Gender" rules={[{ required: true }]}>
-          <Radio.Group>
-            <Radio value="male">male</Radio>
-            <Radio value="Female">Female</Radio>
-          </Radio.Group>
-        </S.StyledFormItem>
-        <S.StyledFormItem name={['user', 'Date of Birth']} label="Date of Birth" rules={[{ required: true }]}>
-          <Col span={8}>
-            <Input placeholder="YYYY/MM/DD" />
-          </Col>
-        </S.StyledFormItem>
-        <S.StyledFormItem name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-          <InputNumber />
-        </S.StyledFormItem>
-        <S.StyledFormItem
+      <S.StyledForm
+        name="nest-messages"
+        onFinish={onFinish}
+        validateMessages={REGISTER_VALIDATE_MESSAGE}
+      >
+        <S.InputWrap label="name">
+          <CustomForm.Input
+            name="last_name"
+            placeholder="last name"
+            rules={[{ required: true }]}
+            style={{ display: 'inline-block', margin: '0px 8px 0px 0px' }}
+          />
+          <CustomForm.Input
+            name="first_name"
+            placeholder="first name"
+            rules={[{ required: true }]}
+            style={{ display: 'inline-block', margin: '0px 8px 0px 0px' }}
+          />
+        </S.InputWrap>
+
+        <CustomForm.Input
+          name="birth_date"
+          label="Date of Birth"
+          placeholder="YYYY/MM/DD"
+          colunms={10}
+          rules={[{ required: true }]}
+        />
+        <CustomForm.Radio
+          name="gender"
+          label="Gender"
+          radioValue={GENDER_OPTION}
+          rules={[{ required: true }]}
+        />
+
+        <S.InputWrap>
+          <CustomForm.Input
+            label="height"
+            name="height"
+            colunms={15}
+            style={{ display: 'inline-block', margin: '0px 8px 0px 0px' }}
+          />
+          <CustomForm.Input
+            label="weight"
+            name="weight"
+            colunms={15}
+            style={{ display: 'inline-block', margin: '0px 8px 0px 0px' }}
+          />
+        </S.InputWrap>
+
+        <CustomForm.Input
           name="phone"
           label="Phone Number"
+          colunms={17}
           rules={[{ required: true, message: 'Please input your phone number!' }]}
-        >
-          <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-        </S.StyledFormItem>
-        <S.StyledFormItem name={['user', 'address']} label="address">
-          <Input />
-        </S.StyledFormItem>
-        <S.StyledFormItem name={['user', 'Facilities']} label="Facilities" rules={[{ required: true }]}>
-          <Col span={14}>
-            <Input />
-          </Col>
-        </S.StyledFormItem>
-        <S.StyledFormItem>
-          <S.StyledButton type="primary" htmlType="submit">
-            register
-          </S.StyledButton>
-        </S.StyledFormItem>
+        />
+        <CustomForm.Input
+          name="recorder_id"
+          label="Recorder id"
+          colunms={18}
+          rules={[{ required: true }]}
+        />
+        <S.ButtonWrap>
+          <CustomForm.Button htmlType="submit">submit</CustomForm.Button>
+        </S.ButtonWrap>
       </S.StyledForm>
     </S.Wrap>
   );
